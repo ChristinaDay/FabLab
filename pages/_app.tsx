1997daaadbf1320fca8353be3ab71bb56165b345
Christina Day <christinamday@gmail.com>
2025-10-10 23:46:07 -0700

    fix(font): also register Oswald in pages/_app to ensure availability on pages router

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,24 @@ import type { AppProps } from 'next/app'
 import '../app/globals.css'
 import { useEffect } from 'react'
 import { supabase } from '@/lib/db'
+import { Oswald } from 'next/font/google'
+
+const oswald = Oswald({
+  variable: '--font-condensed',
+  subsets: ['latin'],
+  weight: ['400','500','600','700']
+})
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     const { data: { subscription } } = supabase.auth.onAuthStateChange(() => {})
     return () => subscription.unsubscribe()
   }, [])
-  return <Component {...pageProps} />
+  return (
+    <div className={oswald.variable}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 
